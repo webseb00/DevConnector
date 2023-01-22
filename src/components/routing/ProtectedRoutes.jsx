5ba34720e1cc8cdd5782c6d30130d32d1429fd5d
webseb00 <lordow91@gmail.com>
2023-01-22 20:53:20 +0100

    Sign user out when session is over

--- src/components/routing/ProtectedRoutes.jsx
+++ src/components/routing/ProtectedRoutes.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { Navigate, Outlet, useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { supabase } from '../../supabaseClient'
+import { signOut } from '../../features/auth/authSlice'
+import { resetUserProfile } from '../../features/profile/profileSlice'
 
 const useAuth = () => {
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const checkSession = async () => {
+      const { data } = await supabase.auth.getSession()
+      
+      if(!data.session) {
+        dispatch(signOut())
+        dispatch(resetUserProfile())
+
+        navigate('/')
+      }
+    }
+
+    checkSession();
+  })
+
   const { user, session } = useSelector(state => state.auth)
   
   if(user && session) {
